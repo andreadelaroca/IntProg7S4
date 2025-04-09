6a0e2f96c54ab9d6fda0c731ff0cbc1bba1fefb1
Naza2006 <nazario@uamv.edu.ni>
2025-04-09 14:31:50 -0600

    Calcular salario del trabajador

---
+++ Ejemplo01.py
@@ -0,0 +1,4 @@
+salario_hora = float(input("Salario Hora: "))
+horas_trabajadas = float(input("Horas Trabajadas: "))
+salario_bruto = salario_hora * horas_trabajadas
+print(f"Salario total dependiendo de las horas trabajadas es de :", salario_bruto)
