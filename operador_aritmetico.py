0a20b9f09eaa58df0d44241e18d0de155248ec4b
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-04-09 10:01:27 -0600

    First commit

---
+++ operador_aritmetico.py
@@ -0,0 +1,5 @@
+#suma de dos numeros
+numero1=int(input("Dime un número: "))
+numero2=int(input("Dime otro número: "))
+suma=numero1+numero2
+print(f"La suma de \n {numero1:>6,} \n+{numero2:>6,} \n= {suma:>6,}")
---
+++ operador_relacional.py
@@ -0,0 +1,11 @@
+num1=25
+num2=25
+
+print(num1>num2)
+print(num1<num2)
+print(num1==num2)
+print(num1!=num2)
+print(num1>=num2)
+print(num1<=num2)
+print(not(num1)==num2)
+print(not(num1))
