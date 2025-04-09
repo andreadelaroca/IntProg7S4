be117aa505bf9440767f5e3d8f369fd39d63fa30
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-04-09 11:23:46 -0600

    Ejercicio 7

---
+++ ej7.py
@@ -0,0 +1,9 @@
+valor1 = float(input("Ingrese el primer valor: "))
+valor2 = float(input("Ingrese el segundo valor: "))
+
+aux = valor1
+valor1 = valor2
+valor2 = aux
+
+print(f"El valor 1 es {valor1} y el valor 2 es {valor2}")
+
