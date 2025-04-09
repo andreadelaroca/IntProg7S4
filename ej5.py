56b5ace52cc1bfca625759ef74f9b7c694391b35
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-04-09 11:00:58 -0600

    Ejercicio 5

---
+++ ej5.py
@@ -0,0 +1,7 @@
+#calcular porcentaje de un número
+
+numero = float(input("Dime un número: "))
+porcentaje = float(input("Dime el porcentaje que deseas saber: "))
+
+resultado = numero*(porcentaje/100)
+print(f"El resultado es {resultado}")
