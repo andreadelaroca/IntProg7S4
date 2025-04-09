7c8848ce24d0a69499daca4fa1c85975209405fa
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-04-09 11:07:46 -0600

    Ejercicio 6

---
+++ ej6.py
@@ -0,0 +1,8 @@
+ano_nacimiento = int(input("Dime tu año de nacimiento: "))
+ano_actual = 2025
+
+edad_actual = ano_actual - ano_nacimiento
+edad_futura = edad_actual+10
+
+print(f"Tu edad actual es {edad_actual}")
+print(f"Tu edad futura es {edad_futura}")
